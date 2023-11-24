6e21c73547d9063ccb8536de7e48a8331420746f
wwolya <o.chupsheva@gmail.com>
2023-11-24 19:45:00 +0300

    second

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 # Словарь для хранения информации о пользователях
 user_info = {
-    1: {'name': 'Иван', 'surname': 'Иванов', 'group': 'Группа 1'},
-    2: {'name': 'Петр', 'surname': 'Петров', 'group': 'Группа 2'},
+    1: {'name': 'Иван', 'surname': 'Иванов', 'group': 'Группа 1', 'chat_id':'123'},
+    2: {'name': 'Петр', 'surname': 'Петров', 'group': 'Группа 2','chat_id': '345'},
 }
 
 @app.route('/', methods=['POST'])
@@ -15,9 +15,10 @@ def receive_data():
     name = data['user_info'].get('name')
     surname = data['user_info'].get('surname')
     group = data['user_info'].get('group')
+    chat_id = data['user_info'].get('chat_id')
 
     new_user_id = max(user_info.keys()) + 1
-    user_info[new_user_id] = {'name': name, 'surname': surname, 'group': group}
+    user_info[new_user_id] = {'name': name, 'surname': surname, 'group': group, 'chat_id':chat_id}
 
     return jsonify({'status': 'success', 'new_user_id': new_user_id})
 
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,6 @@
-import telebot
+from telebot import *
 import requests
+import random
 
 BOT_TOKEN = '6553285113:AAGcCn70J3148zwU6vIR23I0unR-TvQMgsA'
 
@@ -14,20 +15,46 @@ def send_to_server(user_info):
     requests.post(SERVER_URL, json={'user_info': user_info})
 
 
+def code_generation():
+    random_code = random.randint(1000, 9999)
+    return f"Ваш код: {random_code}"
+
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
+    delet = telebot.types.ReplyKeyboardRemove()
     chat_id = message.chat.id
 
     user_info = {
-        #'chat_id': chat_id
-        }
+        # 'chat_id': chat_id
+    }
 
     bot.send_message(chat_id, 'Привет! Я ваш бот. Давайте знакомиться!')
 
     # Запрашиваем имя
-    bot.send_message(chat_id, 'Введите ваше имя:')
+    bot.send_message(chat_id, 'Введите ваше имя:', reply_markup=delet)
     bot.register_next_step_handler(message, process_name_step, user_info)
 
+
+@bot.message_handler(commands=['generation'])
+def generation(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = types.KeyboardButton("Генерация")
+
+    markup.add(btn)
+    bot.send_message(message.chat.id, text="Теперь Вы можете сгенерировать код", reply_markup=markup)
+
+
+@bot.message_handler(commands=['choice'])
+def choice(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Аудитория №1", callback_data="btn1")
+    btn2 = types.InlineKeyboardButton(text="Аудитория №2", callback_data="btn2")
+
+    markup.add(btn1)
+    markup.add(btn2)
+
+    bot.send_message(message.chat.id, text="Выберите аудиторию", reply_markup=markup)
 
 def process_name_step(message, user_info):
     chat_id = message.chat.id
@@ -50,13 +77,35 @@ def process_surname_step(message, user_info):
 def process_group_step(message, user_info):
     chat_id = message.chat.id
     user_info['group'] = message.text
+    user_info['chat_id']=chat_id
 
     # Завершаем регистрацию
-    bot.send_message(chat_id, 'Спасибо за регистрацию! Теперь вы можете отправлять сообщения.')
+    bot.send_message(chat_id, 'Спасибо за регистрацию!')
 
     # Отправляем данные на сервер
     send_to_server(user_info)
+    generation(message)
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+@bot.message_handler(content_types=['text'])
+def get_text(message):
+    if (message.text == 'Генерация'):
+        choice(message)
+        #bot.send_message(message.chat.id)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call: types.CallbackQuery):
+    if call.message:
+        if call.data == "btn1":
+            bot.send_message(call.message.chat.id, code_generation())
+
+        if call.data == "btn2":
+            bot.send_message(call.message.chat.id, code_generation())
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Успешно✅", callback_data="but_2"))
+    bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.id, reply_markup=markup)
+
+
+bot.polling(none_stop=True)
