5999bc05a918095207f291405095cf686279d8da
wwolya <o.chupsheva@gmail.com>
2023-11-24 22:35:59 +0300

    Добавлены две таблицы в базу данных: RegisteredUsers и AccessLog"

--- server.py
+++ server.py
@@ -1,30 +1,73 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+# Создание Flask-приложения
 app = Flask(__name__)
 
-# Словарь для хранения информации о пользователях
-user_info = {
-    1: {'name': 'Иван', 'surname': 'Иванов', 'group': 'Группа 1', 'chat_id':'123'},
-    2: {'name': 'Петр', 'surname': 'Петров', 'group': 'Группа 2','chat_id': '345'},
-}
+# Конфигурация базы данных (используется SQLite в данном случае)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-@app.route('/', methods=['POST'])
+# Инициализация SQLAlchemy
+db = SQLAlchemy(app)
+
+# Определение модели RegisteredUsers для таблицы зарегистрированных пользователей
+class RegisteredUsers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    group = db.Column(db.String(20), nullable=False)
+    chat_id = db.Column(db.String(50), nullable=False)
+    access_logs = db.relationship('AccessLog', backref='user', lazy=True)
+
+# Определение модели AccessLog для таблицы журнала доступа
+class AccessLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('registered_users.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    lock_opened = db.Column(db.Boolean, nullable=False)
+
+# Маршрут для отображения главной страницы
+@app.route('/')
+def main_page():
+    return render_template('main.html')
+
+# Маршрут для отображения списка пользователей
+@app.route('/user_list')
+def user_list():
+    users = RegisteredUsers.query.all()
+    return render_template('user_list.html', users=users, access_logs=None)
+
+# Маршрут для отображения журнала доступа
+@app.route('/access_log')
+def access_log():
+    access_logs = AccessLog.query.all()
+    return render_template('access_log.html', access_logs=access_logs)
+
+# Маршрут для приема данных от телеграм-бота (регистрация нового пользователя и создание записи в журнале доступа)
+@app.route('/receive_data', methods=['POST'])
 def receive_data():
     data = request.json
-
+    # Извлечение данных о пользователе из JSON-запроса
     name = data['user_info'].get('name')
     surname = data['user_info'].get('surname')
     group = data['user_info'].get('group')
     chat_id = data['user_info'].get('chat_id')
 
-    new_user_id = max(user_info.keys()) + 1
-    user_info[new_user_id] = {'name': name, 'surname': surname, 'group': group, 'chat_id':chat_id}
+    # Создание нового пользователя и записи в журнале доступа
+    new_user = RegisteredUsers(name=name, surname=surname, group=group, chat_id=chat_id)
+    db.session.add(new_user)
+    db.session.commit()
 
-    return jsonify({'status': 'success', 'new_user_id': new_user_id})
+    new_access_log = AccessLog(user_id=new_user.id, timestamp=datetime.utcnow(), lock_opened=False)
+    db.session.add(new_access_log)
+    db.session.commit()
 
-@app.route('/')
-def index():
-    return render_template('user_list.html', users=user_info)
+    return jsonify({'status': 'success', 'new_user_id': new_user.id, 'new_access_log_id': new_access_log.id})
 
+# Запуск приложения
 if __name__ == '__main__':
+    with app.app_context():
+        # Создание всех таблиц базы данных при запуске приложения
+        db.create_all()
     app.run(debug=True)
--- telegram_bot.py
+++ telegram_bot.py
@@ -2,24 +2,26 @@ from telebot import *
 import requests
 import random
 
+# Токен вашего телеграм-бота
 BOT_TOKEN = '6553285113:AAGcCn70J3148zwU6vIR23I0unR-TvQMgsA'
 
+# Создание экземпляра телеграм-бота
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # URL вашего сервера, на который будут отправляться данные
-SERVER_URL = 'http://localhost:5000'  # Замените на реальный URL вашего сервера
+SERVER_URL = 'http://localhost:5000/receive_data'  # Замените на реальный URL вашего сервера
 
-
-def send_to_server(user_info):
+# Функция для отправки данных на сервер
+def send_to_server(data):
     # Отправка данных на сервер
-    requests.post(SERVER_URL, json={'user_info': user_info})
+    requests.post(SERVER_URL, json={'user_info': data})
 
-
+# Функция для генерации кода
 def code_generation():
     random_code = random.randint(1000, 9999)
     return f"Ваш код: {random_code}"
 
-
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     delet = telebot.types.ReplyKeyboardRemove()
@@ -35,7 +37,7 @@ def handle_start(message):
     bot.send_message(chat_id, 'Введите ваше имя:', reply_markup=delet)
     bot.register_next_step_handler(message, process_name_step, user_info)
 
-
+# Обработчик команды /generation
 @bot.message_handler(commands=['generation'])
 def generation(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -44,7 +46,7 @@ def generation(message):
     markup.add(btn)
     bot.send_message(message.chat.id, text="Теперь Вы можете сгенерировать код", reply_markup=markup)
 
-
+# Обработчик команды /choice
 @bot.message_handler(commands=['choice'])
 def choice(message):
     markup = types.InlineKeyboardMarkup()
@@ -56,6 +58,7 @@ def choice(message):
 
     bot.send_message(message.chat.id, text="Выберите аудиторию", reply_markup=markup)
 
+# Обработчик ввода имени
 def process_name_step(message, user_info):
     chat_id = message.chat.id
     user_info['name'] = message.text
@@ -64,7 +67,7 @@ def process_name_step(message, user_info):
     bot.send_message(chat_id, 'Введите вашу фамилию:')
     bot.register_next_step_handler(message, process_surname_step, user_info)
 
-
+# Обработчик ввода фамилии
 def process_surname_step(message, user_info):
     chat_id = message.chat.id
     user_info['surname'] = message.text
@@ -73,11 +76,11 @@ def process_surname_step(message, user_info):
     bot.send_message(chat_id, 'Введите номер вашей группы:')
     bot.register_next_step_handler(message, process_group_step, user_info)
 
-
+# Обработчик ввода номера группы
 def process_group_step(message, user_info):
     chat_id = message.chat.id
     user_info['group'] = message.text
-    user_info['chat_id']=chat_id
+    user_info['chat_id'] = chat_id
 
     # Завершаем регистрацию
     bot.send_message(chat_id, 'Спасибо за регистрацию!')
@@ -86,26 +89,22 @@ def process_group_step(message, user_info):
     send_to_server(user_info)
     generation(message)
 
-
+# Обработчик текстовых сообщений
 @bot.message_handler(content_types=['text'])
 def get_text(message):
     if (message.text == 'Генерация'):
         choice(message)
-        #bot.send_message(message.chat.id)
 
-
+# Обработчик инлайн-кнопок
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call: types.CallbackQuery):
     if call.message:
-        if call.data == "btn1":
+        if call.data == "btn1" or call.data == "btn2":
             bot.send_message(call.message.chat.id, code_generation())
 
-        if call.data == "btn2":
-            bot.send_message(call.message.chat.id, code_generation())
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Успешно✅", callback_data="but_2"))
+        bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.id, reply_markup=markup)
 
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Успешно✅", callback_data="but_2"))
-    bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.id, reply_markup=markup)
-
-
+# Запуск бота в режиме long polling
 bot.polling(none_stop=True)
